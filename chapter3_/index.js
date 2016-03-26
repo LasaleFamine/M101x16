69d744a439ad13ba8c844f5ce1243e6564824761
LasaleFamine <alessio.occhipinti@gmail.com>
2016-03-26 18:05:20 +0100

    chapter 3

---
+++ chapter3_/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var wagner = require('wagner-core');
+
+require('./models')(wagner);
+require('./dependencies')(wagner);
+
+var app = express();
+
+app.use(function(req, res, next) {
+	res.append('Access-Control-Allow-Origin', req.headers.origin || '*');
+	res.append('Access-Control-Allow-Credentials', 'true');
+	res.append('Access-Control-Allow-Methods', ['GET', 'OPTIONS', 'PUT', 'POST']);
+	res.append('Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+})
+
+wagner.invoke(require('./auth'), { app: app });
+
+app.use('/api/v1', require('./api')(wagner));
+
+app.listen(3000);
+console.log('Listening on port 3000!');
